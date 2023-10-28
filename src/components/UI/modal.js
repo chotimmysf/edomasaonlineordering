583a34a9580de7c4e0c27565cb9e691d1595bb45
Timmy Cho <chotimmysf@gmail.com>
2023-10-27 18:48:06 -0700

    adding cart context

--- src/components/UI/modal.js
+++ src/components/UI/modal.js
@@ -2,11 +2,11 @@ import { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import classes from './modal.module.css';
 
-const Backdrop = props => {
+const Backdrop = (props) => {
     return <div className={classes.backdrop} onClick={props.onClose}/>
 };
 
-const ModalOverlay = props => {
+const ModalOverlay = (props) => {
     return (<div className={classes.modal}>
         <div className={classes.content}>{props.children}</div>
     </div>
